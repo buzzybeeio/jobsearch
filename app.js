e611f17753cbcff138e9d28014c4015609f5f15d
Rai Lee <thisisrailee@gmail.com>
2017-07-08 11:31:28 -0700

    changed cors settings

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const port = process.env.PORT || 3000
 
 const jobObject = {}
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (request, response) => {
     queryCommands.getAllJobs()
         .then( results => {
