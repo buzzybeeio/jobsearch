c2578b2993188dbab739ec2b64a91a088180285c
Hernan Mendez <hernan21112001@gmail.com>
2017-10-14 10:18:31 -0400

    Fixed findStory, added Routers

--- app.js
+++ app.js
@@ -4,47 +4,14 @@ const port = process.env.PORT || 3000;
 const stories = require('./database/mongoose').stories;
 const jobs = require('./database/mongoose').jobs;
 
-const jobObject = {}
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.get('/', (request, response) => {
-  jobs.find({}, null, { sort: '-datepost', limit: 50 }, (err, docs) => {
-    if (err) response.json([])
-    else response.json(docs)
-  })
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
 })
 
-app.get('/stories', (request, response) => {
-  stories.find({}, null, { sort: '-date', limit: 8 }, (err, docs) => {
-    if (err) response.json([])
-    else response.json(docs)
-  })
-})
-
-app.get('/story/:storyID', (request, response) => {
-  stories.findById(request.params.storyID, (err, story) => {
-    if (err) response.json({})
-    else response.json(story)
-  })
-})
-
-app.get('/findStory/:str', () => {
-  const words = req.params.str.split('%20')
-  const criteria = {
-    $or: words.map(word => ({})),
-    limit: 20
-  }
-
-  stories.find(criteria, 'name', (err, stories) => {
-    if (err) response.json({})
-    else response.json(stories)
-  })
-})
+app.use(require('./Routes/Jobs'))
+app.use(require('./Routes/Stories'))
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
