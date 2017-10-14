b14f0de98837f8485985f7b4be48a98ece63f6b8
Hernan Mendez <hernan21112001@gmail.com>
2017-10-14 10:27:30 -0400

    Removed Mongoose's callbacks, now using promises

--- app.js
+++ app.js
@@ -15,8 +15,13 @@ app.use(require('./Routes/Stories'))
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
-  jobs.remove({}, () => {
+  jobs.remove({})
+  .then(() => {
     require('./data-resources/authenticjobs')
     require('./data-resources/indeed')
   })
+  .catch(err => {
+    console.log('error removing')
+    console.log(err)
+  })  
 })
--- worker.js
+++ worker.js
@@ -1,6 +1,11 @@
 const jobs = require('./database/mongoose').jobs
 
-jobs.remove({}, () => {
-  require('./data-resources/authenticjobs')
-  require('./data-resources/indeed')
-})
+jobs.remove({})
+  .then(() => {
+    require('./data-resources/authenticjobs')
+    require('./data-resources/indeed')
+  })
+  .catch(err => {
+    console.log('error removing')
+    console.log(err)
+  })
