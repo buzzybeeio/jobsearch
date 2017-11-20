9cfdb8a8641864cf6cb6ed9ffb619793d3493dc2
Hernan Mendez <hernan21112001@gmail.com>
2017-11-20 17:28:41 -0400

    Added Login

--- Routes/Users.js
+++ Routes/Users.js
@@ -4,20 +4,20 @@ const bcrypt = require('bcrypt')
 
 router.post('/register', (req, res) => {
   bcrypt.hash(req.body.password, 15, (err, hash) => {
-    if(err){
+    if (err) {
       console.log(err)
       res.json({ err: true })
     } else {
-      const object = { 
+      const object = {
         name: req.body.name,
         password: hash,
         email: req.body.email,
         jobs: []
       }
-      users.findOne({ name: object.name, email: object.email})
+      users.findOne({ email: object.email })
         .then(result => {
-          if(result) {
-            res.json({ err: true, errMessage: 'Username or email Taken'})
+          if (result) {
+            res.json({ err: true, errMessage: 'Email Taken' })
           } else {
             users.save(object)
               .then(() => res.json({ err: false }))
@@ -27,4 +27,28 @@ router.post('/register', (req, res) => {
         .catch(() => res.json({ err: true }))
     }
   })
+})
+
+router.post('/login', (req, res) => {
+  users.findOne({ email: req.body.email })
+    .then(result => {
+      if (result) {
+        bcrypt.compare(req.body.password, result.password, (err, res) => {
+          if (err) {
+            res.json({ err: true })
+          } else if (res) {
+            const response = {
+              name: result.name,
+              jobs: result.jobs
+            }
+            res.json(response)
+          } else {
+            res.json({ err: true, errMessage: 'Wrong email or password' })
+          }
+        })
+      } else {
+        res.json({ err: true, errMessage: 'Wrong email or password' })
+      }
+    })
+    .catch(() => res.json({ err: true }))
 })
