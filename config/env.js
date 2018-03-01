0485d31b29ced28739a005365b18e31170631e3d
Hernan Mendez <hernan21112001@gmail.com>
2018-03-01 15:44:03 -0500

    Detail

--- config/env.js
+++ config/env.js
@@ -1,9 +1,8 @@
-const { randomBytes } = require('crypto')
 module.exports = {
   Mongo_url: process.env.MONGODB_URI || 'mongodb://localhost/buzzybee',
   PORT: process.env.PORT || 4000,
   OFFLINE: process.env.OFFLINE || false,
-  secret: (randomBytes(50).toString('base64') || process.env.BACKUPSECRET) || 'rm3n2huyfeuw-+jh+2dfvedsiuvc3213grhry8uweikr3n9++jgr=jrgwcJeeegufodsough48',
+  secret: process.env.SECRET || 'rm3n2huyfeuw-+jh+2dfvedsiuvc3213grhry8uweikr3n9++jgr=jrgwcJeeegufodsough48',
   EMAIL: process.env.EMAIL || 'welcome@buzzybee.io',
   PASS: process.env.PASS || 'guest123'
 }
